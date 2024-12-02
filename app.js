a5759c7354104877fffff21db66ec6376dc201ff
Suzan BULGAN <sbulgan@prismamedia.com>
2024-12-02 14:51:29 +0100

    feature/body-parser update <app> méthode post ajoutée

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res)=>{
     res.send("Hello World")
 })
 
-
 app.get('/', (req, res)=>{
     res.send(`Current date and time : ${new Date()}`)
+})
+
+app.post('/users', (req, res) => {
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.status(201).json(newUser)
 })
 
 app.listen(port, () => {
