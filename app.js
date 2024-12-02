278968cd377acd80b640a9f528897b30ad605a0d
Suzan BULGAN <sbulgan@prismamedia.com>
2024-12-02 14:31:07 +0100

    Feature/date-route update : ajout de la ligne supprimée

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
+    res.send("Hello World")
+})
+
+
+app.get('/', (req, res)=>{
     res.send(`Current date and time : ${new Date()}`)
 })
 
